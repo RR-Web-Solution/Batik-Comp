be824742caf1c7f9b86ea4a0928d317c110d6e57
Rendy <rendyzz999@gmail.com>
2026-08-19 16:19:30 +0700

    Fix Bootstrap SRI integrity hash on public pages
    The sha384 hash for bootstrap.bundle.min.js had a typo (NNkmxc5 vs NNkmXc5),
    causing browsers to block Bootstrap JS via SRI on products, category, order
    success and track pages. Aligns with the official Bootstrap 5.3.3 hash.

--- resources/views/order/track.blade.php
+++ resources/views/order/track.blade.php
@@ -140,7 +140,7 @@
         </section>
     </main>
     <!-- Bootstrap 5.3 JS Bundle with Popper -->
-    <script crossorigin="anonymous" integrity="sha384-YvpcrYf0tY3lHB60NNkmxc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    <script crossorigin="anonymous" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
